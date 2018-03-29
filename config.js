e39acc97267e6121924376b4074cc3f60d256e69
Subhasish Dutta <sdutta@uplandsoftware.com>
2018-03-29 18:02:26 +0530

    db string changes

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
 	'admintoken' : 'U2FsdGVkX19v0jsoa/94adkW34+Ro8j/aTnHIBhO6U8=',
     'key' : 'GhPMvymzAN',
     'secret': 'intellirioconsultanylabs',
-    'database': 'mongodb://admin:admin@intelliriolabs-shard-00-00-fsnjl.mongodb.net:27017,intelliriolabs-shard-00-01-fsnjl.mongodb.net:27017,intelliriolabs-shard-00-02-fsnjl.mongodb.net:27017/test?ssl=true&replicaSet=intelliriolabs-shard-0&authSource=admin'
+    'database': 'mongodb://admin:photography88@healthyfling-shard-00-00-yiwsj.mongodb.net:27017,healthyfling-shard-00-01-yiwsj.mongodb.net:27017,healthyfling-shard-00-02-yiwsj.mongodb.net:27017/test?ssl=true&replicaSet=healthyfling-shard-0&authSource=admin'
 };
