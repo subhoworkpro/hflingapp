ea2a607bab1eed5f57e6f42e3f86b6859f90950b
Subhasish Dutta <sdutta@uplandsoftware.com>
2018-03-31 17:29:56 +0530

    adding local db

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
 	'admintoken' : 'U2FsdGVkX19v0jsoa/94adkW34+Ro8j/aTnHIBhO6U8=',
     'key' : 'GhPMvymzAN',
     'secret': 'intellirioconsultanylabs',
-    'database': 'mongodb://admin:admin@localhost:27017'
+    'database': 'mongodb://admin:admin@localhost:27017/hflingdb'
 };
