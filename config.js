3bdfcc2f39a3740121426d730f9433cf83255dca
Subhashish Dutta <sdutta@uplandsoftware.com>
2018-06-26 10:49:19 +0530

    UpdateS

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
 	'admintoken' : 'U2FsdGVkX19v0jsoa/94adkW34+Ro8j/aTnHIBhO6U8=',
     'key' : 'GhPMvymzAN',
     'secret': 'intellirioconsultanylabs',
-    'database': 'mongodb://hflingadmin:photography88@localhost:27017/admin'
+    'database': 'mongodb://localhost:27017/hflingdb2'
 };
