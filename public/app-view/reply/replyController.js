df08dffac328ef2aee500d66507eec91da1c7f13
Subhashish Dutta <sdutta@uplandsoftware.com>
2019-05-03 18:14:34 +0530

    Fix menubar issue

--- public/app-view/reply/replyController.js
+++ public/app-view/reply/replyController.js
@@ -207,7 +207,7 @@ app.controller('ReplyController', ['$rootScope','$scope', '$location', 'HttpServ
             var options = { timeZone: "America/New_York"}
             var estTime = new Date();
             var data = {
-                "htmlmessage": "<p>"+$scope.replymessage +"</p><p>Original Post:</p><p>"+ $location.absUrl().replace("reply","detail")+"</p> Regards, <br/>Healthyfling Team<p>If this message is spam, you can always block this email so you no longer recieve their messages. You need to do this through your email provider!</p>",
+                "htmlmessage": "<p>"+$scope.replymessage +"</p><p>Original Post:</p><p>"+ $location.absUrl().replace("reply","detail")+"</p> Regards, <br/>Healthyfling Team<p>If you feel this message is spam or an advertisement, you can always block or report this email through your email provider. If this message is illegal or associated with anything illegal, PLEASE CONTACT THE AUTHORITIES!</p>",
                 "subject": "[HealthyFling] RE: "+( $scope.title || $scope.message)+" - "+estTime.toLocaleString("en-US", options)+" ET",
                 "sender1": $scope.sender1,
                 "attachments": $rootScope.tempImageList,
